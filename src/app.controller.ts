e9257666fd459dec446ff58875db3de785decf11
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-04-14 22:56:01 +0530

    loadtest added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/fibonacci')
+  async getFibo() {
+    return this.appService.fibonacci(40);
   }
 }
